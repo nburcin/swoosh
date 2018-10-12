f677b8da1739e8ada48285287c8dfe45a06ec722
Nick Burcin <nicholas.burcin@outlook.com>
2018-10-12 09:37:47 -0400

    Connected and created custom viewcontrollers

---
+++ Swoosh/Swoosh/Controller/SkillVC.swift
@@ -0,0 +1,30 @@
+//
+//  SkillVC.swift
+//  Swoosh
+//
+//  Created by Nicholas Burcin on 10/12/18.
+//  Copyright © 2018 Burcin Software. All rights reserved.
+//
+
+import UIKit
+
+class SkillVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
